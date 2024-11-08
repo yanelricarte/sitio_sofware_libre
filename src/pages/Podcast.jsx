5e8831520b6652884a91776ab4da2b20e6dbdcef
yricarte <ryanel@indracompany.com>
2024-11-08 18:45:42 -0300

    despliegue

--- src/pages/Podcast.jsx
+++ src/pages/Podcast.jsx
@@ -1,11 +1,16 @@
-import React from 'react';
-import './Podcast.css';  // Asegúrate de agregar los estilos necesarios
+import React from "react";
+import "./Podcast.css"; //
+import imgLinkedinYr from "../assets/img/yanel_ricarte.jfif";
+import imgLinkedinBa from "../assets/img/brenda_alvarez.jfif";
 
 const Podcast = () => {
   return (
     <section className="content">
       <h3>Podcast</h3>
-      <p>Escucha los episodios de nuestro podcast sobre la temática de comunicación digital.</p>
+      <p>
+        Escucha los episodios de nuestro podcast sobre la temática de
+        comunicación digital.
+      </p>
 
       <div className="podcast-cards-container">
         {/* Episodio 1 */}
@@ -25,7 +30,10 @@ const Podcast = () => {
         {/* Episodio 2 */}
         <div className="podcast-card">
           <h4>Episodio 2</h4>
-          <p>En este episodio, profundizamos en temas clave sobre comunicación digital.</p>
+          <p>
+            En este episodio, profundizamos en temas clave sobre comunicación
+            digital.
+          </p>
           <a
             href="https://on.soundcloud.com/kZjhdERJ1UjmEByR8"
             target="_blank"
@@ -39,7 +47,9 @@ const Podcast = () => {
         {/* Episodio 3 */}
         <div className="podcast-card">
           <h4>Episodio 3</h4>
-          <p>No te pierdas nuestro tercer episodio, ¡lleno de contenido valioso!</p>
+          <p>
+            No te pierdas nuestro tercer episodio, ¡lleno de contenido valioso!
+          </p>
           <a
             href="https://on.soundcloud.com/dEs21zFFC2fhcVvY9"
             target="_blank"
@@ -48,6 +58,69 @@ const Podcast = () => {
           >
             Escuchar en SoundCloud
           </a>
+        </div>
+      </div>
+
+      {/* Información sobre las creadoras */}
+      <div className="creators-info">
+        <h3>Las creadoras de este podcast:</h3>
+        {/* Creadora 1 */}{" "}
+        <div className="creator">
+          <div className="creator-header">
+            <img
+              src={imgLinkedinBa}
+              alt="Brenda Patricia Alvarez"
+              className="creator-image"
+            />
+            <div className="creator-details">
+              <p>
+                <strong>Brenda Patricia Alvarez</strong>
+              </p>
+              <p>
+                Profesora de Lengua Inglesa
+                <span className="sub-details">Coordinadora académica</span>
+              </p>
+
+              <a
+                href="https://www.linkedin.com/in/brenda-patricia-alvarez-westtesten-4b65342b7/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="creator-link"
+              >
+                <i className="fab fa-linkedin"></i> Ver perfil de LinkedIn
+              </a>
+            </div>
+          </div>
+        </div>
+        {/* Creadora 2 */}
+        <div className="creator">
+          <div className="creator-header">
+            <img
+              src={imgLinkedinYr}
+              alt="Yanel Ricarte"
+              className="creator-image"
+            />
+            <div className="creator-details">
+              <p>
+                <strong>Yanel Ricarte</strong>
+              </p>
+              <p>
+                {" "}
+                Desarrolladora de software
+                <span className="sub-details">
+                  Docente Especialista en TIC
+                </span>{" "}
+              </p>
+              <a
+                href="https://www.linkedin.com/in/yanelricarte/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="creator-link"
+              >
+                <i className="fab fa-linkedin"></i> Ver perfil de LinkedIn
+              </a>
+            </div>
+          </div>
         </div>
       </div>
     </section>
