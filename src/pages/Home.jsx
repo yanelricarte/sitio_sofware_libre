46e634beaeaac08be723814b07080cb2e035d14b
yricarte <ryanel@indracompany.com>
2024-11-08 16:52:21 -0300

    despliegue

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import React from "react";
+import imgHome from "../assets/img/woman.jpeg";
 
 const Home = () => {
   return (
-    <section className="content">
-      <h3>Bienvenidos al Proyecto</h3>
-      <p>Este sitio web ha sido creado como parte del Trabajo Integrador Final II, en el cual se integran diversos recursos de comunicación digital. Navega a través de las diferentes secciones para ver cada uno de los recursos.</p>
-    </section>
+    <>
+      <section className="content">
+        <div>
+          <h3>Bienvenidos al Proyecto</h3>
+          <p>
+            Este sitio web ha sido creado como parte del Trabajo Integrador
+            Final II, en el cual se integran diversos recursos de comunicación
+            digital. Navega a través de las diferentes secciones para ver cada
+            uno de los recursos.
+          </p>
+        </div>
+        <div className="home-img">
+          <img src={imgHome} alt="Imagen Home" />
+        </div>
+      </section>
+    </>
   );
 };
 
