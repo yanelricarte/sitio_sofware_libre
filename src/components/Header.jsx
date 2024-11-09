c0577c1bfa5292a534a3eaff174493dfc6d0f92b
yricarte <ryanel@indracompany.com>
2024-11-09 00:05:59 -0300

    despliegue

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './Header.css';  
-import logo from '../assets/img/logo-utn.png'; 
+import logo from '../assets/img/logo.png'; 
 
 const Header = () => {
   return (
     <header className="header">
       <div className="header-container">
         <a href="/">  
-          <img src={logo} className="logo" alt="Logo UTN" /> 
+          <img src={logo} className="logo" alt="Logo" /> 
         </a>
         <h2 className="subtitle">Gestión de Recursos Digitales con Software Libre</h2>
       </div>
@@ -20,6 +20,8 @@ const Header = () => {
           <li><a href="/video">Video</a></li>
         </ul>
       </nav>
+
+
     </header>
   );
 };
